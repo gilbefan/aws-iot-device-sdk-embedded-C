edeb0d58b100b6f582f09c802695d0b5ccfd0ac6
Fan <gilbefan@amazon.com>
2021-07-13 08:52:35 -0700

    dded clock to zephyr transport library for organization purposes for now, does not actually use this clock file yet for the demo.

--- platform/zephyr/zephyrFilePaths.cmake
+++ platform/zephyr/zephyrFilePaths.cmake
@@ -1,11 +1,11 @@
 set( SOCKETS_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/src/sockets_zephyr.c )
+     ${CMAKE_CURRENT_LIST_DIR}/transport/src/sockets_zephyr.c )
 
 set( PLAINTEXT_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/src/plaintext_zephyr.c )
+     ${CMAKE_CURRENT_LIST_DIR}/transport/src/plaintext_zephyr.c )
 
 set( MBEDTLS_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/src/mbedtls_zephyr.c )
+     ${CMAKE_CURRENT_LIST_DIR}/transport/src/mbedtls_zephyr.c )
 
 set( COMMON_TRANSPORT_INCLUDE_PUBLIC_DIRS
-     ${CMAKE_CURRENT_LIST_DIR}/include )
+     ${CMAKE_CURRENT_LIST_DIR}/transport/include )
